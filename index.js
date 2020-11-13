f6235ef3b361ce86ab571f72eaf9bbb0b7148e54
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-13 16:47:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 main.remove();
 let newHeader = document.querySelector('Body');
-newHeader.innerHTML= '<h1 id="victory ">Jana is the chamion</h1>';
+newHeader.innerHTML= '<h1 id="victory">Jana is the chamion</h1>';
