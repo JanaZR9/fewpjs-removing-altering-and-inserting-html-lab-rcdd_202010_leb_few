d5c0e5c919bf9617d8ce1ed058d343f2292dc7ac
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-13 16:35:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 // Write your code here!
-let a= document.querySelector('main')
-let i = document.getgetElementById("#main");
-i.remove();
-a.remove();
+main.removeChild(main.document.getgetElementById("#main"));
+i
