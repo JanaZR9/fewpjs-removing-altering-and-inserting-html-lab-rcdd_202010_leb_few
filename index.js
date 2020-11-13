dae5a7b9a09e3316c2f0ea163ef3ec7e8fdf6e19
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-13 16:51:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ main.remove();
 //newHeader.innerHTML= '<h1 id="victory">Jana is the chamion</h1>';
 var newHeader = document.createElement("h1");
 newHeader.innerHTML = "Jana is the champion";
-document.body.appendChild(newHeader);           
+document.body.appendChild(newHeader);  
+
+document.querySelector('h1').add('#victory');
