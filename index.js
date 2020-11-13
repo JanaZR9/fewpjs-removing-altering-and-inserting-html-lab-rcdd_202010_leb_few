1e5d30359069ce7e032c569de23cfd2340f33af3
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-13 16:43:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 main.remove();
-let newHeader = document.createElement('h1 #victory');
-document.body.appendChild(newHeader);
+let newHeader = document.querySelector('body');
+newHeader.innerHTML= '<h1 id="victory "></h1>';
