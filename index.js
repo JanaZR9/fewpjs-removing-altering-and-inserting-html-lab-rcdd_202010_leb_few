d29bc9d72352606fe781d955152ec2ad62530c19
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-13 16:48:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Write your code here!
 main.remove();
-let newHeader = document.querySelector('Body');
-newHeader.innerHTML= '<h1 id="victory">Jana is the chamion</h1>';
+//let newHeader = document.querySelector('Body');
+//newHeader.innerHTML= '<h1 id="victory">Jana is the chamion</h1>';
+var newHeader = document.createElement("h1#victory");   // Create a <button> element
+newHeader.innerHTML = "Jana is the champion";                   // Insert text
+document.body.appendChild(newHeader);               // Append <button> to <body>
